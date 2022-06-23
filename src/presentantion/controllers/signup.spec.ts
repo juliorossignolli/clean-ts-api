be131079857cc4490af77d6cda311fcc48684be0
Julio Rossignolli <jcrossignolli@yahoo.com.br>
2022-06-23 17:57:16 -0300

    refactor: move server error to a helper method

--- src/presentantion/controllers/signup.spec.ts
+++ src/presentantion/controllers/signup.spec.ts
@@ -1,8 +1,6 @@
 import { SignUpController } from './signup'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParameterError } from '../errors/invalid-param-error'
+import { MissingParamError, InvalidParameterError, ServerError } from '../errors'
 import { EmailValidator } from '../protocols/email-validator'
-import { ServerError } from '../errors/server-error'
 
 interface SutTypes {
   sut: SignUpController
