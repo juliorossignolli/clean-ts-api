00381417038882de07fefb9e82cd794a3606b6af
Julio Rossignolli <jcrossignolli@yahoo.com.br>
2022-06-24 14:35:43 -0300

    feat: ensure SignUpController if valid data is provided

--- src/presentantion/controllers/signup/signup.ts
+++ src/presentantion/controllers/signup/signup.ts
@@ -30,7 +30,7 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParameterError('email'))
       }
 
-      this.addAccount.add({
+      const account = this.addAccount.add({
         name,
         email,
         password
@@ -38,7 +38,7 @@ export class SignUpController implements Controller {
 
       return {
         statusCode: 200,
-        body: 'Success'
+        body: account
       }
     } catch (error) {
       console.error(error)
